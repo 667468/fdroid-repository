e74f262f7238bc27819a3fe81ab72249bd05a01d
Ricki Hirner <hirner@bitfire.at>
2017-08-16 13:29:58 +0200

    Switch to OpenJDK image

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 repo_url = "https://rfc2822.pages.io/fdroid-firefox/"
 repo_name = "Firefox F-Droid repo (inofficial)"
+repo_icon = "fdroid-icon.png"
 repo_description = """
 Firefox
 """
 
-archive_older = 3
+archive_older = 0
 
+repo_keyalias = "not_required"
+keystore = "not_required.bks"
+keystorepass = "this-is-never-used"
+keypass = "this-is-never-used"
+
+local_copy_dir = "/fdroid"
